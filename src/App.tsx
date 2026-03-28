f8d657a8e7dc87ef2fa82861a3301f7632d1debb
tomask411 <tomask400@gmail.com>
2026-03-28 19:14:05 +0300

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -202,6 +202,8 @@ export default function App() {
           });
         }
       } catch {}
+    }, (err) => {
+      console.log('GPS denied or unavailable:', err.message);
     });
   }, []);
 
